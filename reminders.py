9c7c44c40cb5b5ae488bf7e92f5d89fc631b834e
Faye Ly <fayedotly@gmail.com>
2025-10-15 16:37:02 -0700

    written some beginning code in python file

--- reminders.py
+++ reminders.py
@@ -1,1 +1,12 @@
 import os
+import sqlite3
+
+db_path = os.path.join(os.path.expanduser('~'), 'projects', 'reminders', 'reminders.db')
+conn = sqlite5.connect(db_path)
+cur = conn.cursor()
+cur2 = conn.cursor()
+information = []
+upcoming = []
+
+cur.execute('select * from dates')
+cur2.execute('select * from days_until')
